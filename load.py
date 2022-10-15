2ff7411b1f95190925231d9eef9f0e775f6532be
NOTEBOOK-L07\user <terenevichlollegend@gmail.com>
2022-10-15 15:15:29 +0300

    узыка звуки анимации и меню

--- load.py
+++ load.py
@@ -1,5 +1,6 @@
 import pygame
-player_image=pygame.image.load("image/player/player.png").convert_alpha()
+player_image=[pygame.image.load("image/player/player 1.png").convert_alpha(),
+              pygame.image.load("image/player/player 2.png").convert_alpha()]
 player_bullet=pygame.image.load("image/player/player bullet.png").convert_alpha()
 enemy_image=pygame.image.load("image/enemy/enemy.png").convert_alpha()
 enemy_bullet=pygame.image.load("image/enemy/enemy bullet.png").convert_alpha()
@@ -8,4 +9,14 @@ iron_image=pygame.image.load("image/blocks/iron block.png").convert_alpha()
 bush_image=pygame.image.load("image/blocks/bush block.png").convert_alpha()
 water_image=pygame.image.load("image/blocks/water block.png").convert_alpha()
 flag_image=pygame.image.load("image/blocks/flag.png").convert_alpha()
-game_over=pygame.image.load("image/dop/game over.png").convert_alpha()
+start_image=pygame.image.load("image/dop/start.png").convert_alpha()
+exit_image=pygame.image.load("image/dop/exit.png").convert_alpha()
+records_image=pygame.image.load("image/dop/records.png").convert_alpha()
+menu_image=pygame.image.load("image/dop/fon.png").convert_alpha()
+back_image=pygame.image.load("image/dop/back.png").convert_alpha()
+bullet_image=[pygame.image.load("image/dop/1.png").convert_alpha(),
+              pygame.image.load("image/dop/2.png").convert_alpha(),
+              pygame.image.load("image/dop/3.png").convert_alpha(),
+              pygame.image.load("image/dop/4.png").convert_alpha()]
+shot_sound=pygame.mixer.Sound("sound/shot.mp3")
+boom_sound=pygame.mixer.Sound("sound/boom.mp3")
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import os
 import sys
 import random
 
-file=r'sound/bleach-blich-tema-ichigo.mp3'
+file=r'sound/tanchiki-tanchiki-mp3.mp3'
 pygame.mixer.init()
 track = pygame.mixer.music.load(file)
+pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play()
 pygame.font.init()
 pygame.init()
@@ -14,12 +15,45 @@ os.chdir(current_path)
 WIDTH=1200
 HEIGHT=800
 FPS=60
+record=0
 sc=pygame.display.set_mode((WIDTH, HEIGHT))
 clock=pygame.time.Clock()
-lvl="Game"
+lvl="menu"
 
 from load import *
 
+
+def startMenu():
+    global lvl
+    sc.blit(menu_image, (0, 0))
+    sc.blit(start_image, (100, 200))
+    sc.blit(records_image, (100, 300))
+    sc.blit(exit_image, (100, 400))
+    pos_mouse=pygame.mouse.get_pos()
+    if pygame.mouse.get_pressed()[0]:
+        if 100<pos_mouse[0]<400:
+            if 200<pos_mouse[1]<275:
+                restart()
+                drawMaps("1.txt")
+                lvl="Game"
+            elif 300<pos_mouse[1]<375:
+                lvl="record"
+                sc.fill("black")
+                f1 = pygame.font.Font(None, 36)
+                text1 = f1.render("Ваш рекорд: 0", 1, (180, 180, 180))
+                sc.blit(text1, (500, 400))
+                sc.blit(back_image, (1100, 50))
+                pos_mouse_2 = pygame.mouse.get_pos()
+                if pygame.mouse.get_pressed()[0]:
+                    if 1100 < pos_mouse_2[1] < 1150:
+                        print("jnkm")
+                        if 50 < pos_mouse_2[1] < 100:
+                            lvl="menu"
+                            print("hdvbn")
+            elif 400<pos_mouse[1]<475:
+                pygame.quit()
+                sys.exit()
+    pygame.display.update()
 
 def lvlGame():
     sc.fill("black")
@@ -152,31 +186,51 @@ class Player(pygame.sprite.Sprite):
         self.speed=5
         self.dir="top"
         self.timer_shot=0
+        self.frame=0
+        self.timer_anime=0
+        self.anime=False
 
     def update(self):
+        self.anime = False
         self.timer_shot+=1
         key=pygame.key.get_pressed()
         if key[pygame.K_SPACE] and self.timer_shot>40:
             bullet=Bullet_player(player_bullet, self.rect.center, self.dir)
             bullet_player_group.add(bullet)
             self.timer_shot=0
+            shot_sound.play()
         if key[pygame.K_a]:
-            self.image=pygame.transform.rotate(player_image, 90)
+            self.image=pygame.transform.rotate(player_image[self.frame], 90)
             self.rect.x-=self.speed
             self.dir="left"
+            self.anime = True
         elif key[pygame.K_d]:
-            self.image=pygame.transform.rotate(player_image, -90)
+            self.image=pygame.transform.rotate(player_image[self.frame], -90)
             self.rect.x+=self.speed
             self.dir="right"
+            self.anime = True
         elif key[pygame.K_w]:
-            self.image=pygame.transform.rotate(player_image, 0)
+            self.image=pygame.transform.rotate(player_image[self.frame], 0)
             self.rect.y-=self.speed
             self.dir="up"
+            self.anime = True
         elif key[pygame.K_s]:
-            self.image=pygame.transform.rotate(player_image, 180)
+            self.image=pygame.transform.rotate(player_image[self.frame], 180)
             self.rect.y+=self.speed
             self.dir="down"
-        pygame.sprite.groupcollide(bullet_enemy_group, player_group, True, True)
+            self.anime = True
+        if self.anime:
+            self.timer_anime+=1
+            if self.timer_anime/FPS>0.1:
+                if self.frame==len(player_image)-1:
+                    self.frame=0
+                else:
+                    self.frame+=1
+                self.timer_anime=0
+        if pygame.sprite.groupcollide(bullet_enemy_group, player_group, True, True):
+            global lvl
+            lvl="menu"
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, image, pos):
         pygame.sprite.Sprite.__init__(self)
@@ -248,7 +302,6 @@ class Enemy(pygame.sprite.Sprite):
                 self.dir="right"
             elif self.dir=="right":
                 self.dir="left"
-        pygame.sprite.groupcollide(bullet_player_group, enemy_group, True, True)
 
 class Bullet_player(pygame.sprite.Sprite):
     def __init__(self, image, pos, dir):
@@ -259,6 +312,10 @@ class Bullet_player(pygame.sprite.Sprite):
         self.rect.y=pos[1]
         self.speed=5
         self.dir=dir
+        self.anime=False
+        self.timer_anime=0
+        self.frame=0
+        self.boom=False
 
     def update(self):
         if self.dir=="up":
@@ -269,6 +326,26 @@ class Bullet_player(pygame.sprite.Sprite):
             self.rect.x += self.speed
         elif self.dir=="left":
             self.rect.x -= self.speed
+        if pygame.sprite.spritecollide(self, enemy_group, True):
+            global record
+            self.anime=True
+            self.speed=0
+            self.boom = True
+            record+=1
+        if self.anime:
+            self.timer_anime+=1
+            if self.timer_anime/FPS>0.1:
+                if self.frame==len(bullet_image)-1:
+                    self.frame=0
+                    self.rect.center=(-1000, 0)
+                    self.kill()
+                else:
+                    self.frame+=1
+                self.timer_anime=0
+            self.image=bullet_image[self.frame]
+        if self.boom:
+            boom_sound.play()
+            self.boom=False
 
 class Bullet_enemy(pygame.sprite.Sprite):
     def __init__(self, image, pos, dir):
@@ -298,32 +375,33 @@ class Flag(pygame.sprite.Sprite):
         self.rect.x=pos[0]
         self.rect.y=pos[1]
     def update(self):
-        pygame.sprite.groupcollide(bullet_player_group, flag_group, True, True)
-        pygame.sprite.groupcollide(bullet_enemy_group, flag_group, True, True)
+        global lvl
+        if pygame.sprite.groupcollide(bullet_player_group, flag_group, True, True):
+            lvl="menu"
+        if pygame.sprite.groupcollide(bullet_enemy_group, flag_group, True, True):
+            lvl="menu"
+
+class restart():
+    global bullet_enemy_group
+    global water_group, brick_group, bush_group, iron_group
+    global player_group, enemy_group, flag_group, bullet_player_group, player
+    brick_group = pygame.sprite.Group()
+    bush_group = pygame.sprite.Group()
+    iron_group = pygame.sprite.Group()
+    water_group = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    enemy_group = pygame.sprite.Group()
+    flag_group = pygame.sprite.Group()
+    bullet_player_group = pygame.sprite.Group()
+    bullet_enemy_group = pygame.sprite.Group()
+    player=Player(player_image[0], (400, 640))
+    player_group.add(player)
 
 
 
 
 
 
-brick_group=pygame.sprite.Group()
-bush_group=pygame.sprite.Group()
-iron_group=pygame.sprite.Group()
-water_group=pygame.sprite.Group()
-player_group=pygame.sprite.Group()
-enemy_group=pygame.sprite.Group()
-bullet_player_group=pygame.sprite.Group()
-bullet_enemy_group=pygame.sprite.Group()
-flag_group=pygame.sprite.Group()
-player=Player(player_image, (560, 600))
-player_group.add(player)
-
-#f1 = pygame.font.Font(None, 36)
-#text1 = f1.render('GAME OVER', True,(180, 0, 0))
-
-#sc.blit(text1, (600, 400))
-
-drawMaps('1.txt')
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -331,4 +409,6 @@ while True:
             sys.exit()
     if lvl=="Game":
         lvlGame()
+    elif lvl=="menu":
+        startMenu()
     clock.tick(FPS)
